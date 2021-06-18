040ce4ce082d22ed43427a8342c7b06b0ab2afb7
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2021-06-18 20:40:11 +0200

    refactor: :recycle: split up into two files

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
+mod caesar;
+
 use std::io;
-
-fn generate_alphabet() -> Vec<char> {
-    let alphabet = ('a'..='z') // Start as u8
-        .map(|c| c as char) // Convert all to chars
-        .filter(|c| c.is_alphabetic()) // Filter only alphabetic chars
-        .collect::<Vec<_>>(); // Collect as Vec<char>
-
-    return alphabet;
-}
-
-fn find_letter_index_in_alphabet(letter: char, alphabet: Vec<char>) -> Option<usize> {
-    alphabet.iter().position(|&x| x == letter)
-}
-
-fn find_shifted_letter(letter: char, shift: u32, alphabet: Vec<char>) -> char {
-    let index = find_letter_index_in_alphabet(letter, alphabet.clone());
-    match index {
-        Some(i) => {
-            let shifted_index = i + shift as usize;
-            let safe_index = if shifted_index > alphabet.len() {
-                alphabet.len() - i
-            } else {
-                shifted_index
-            };
-            return alphabet[safe_index];
-        }
-        None => panic!("No index!"),
-    }
-}
 
 enum Cipher {
     _Caesar,
@@ -35,19 +8,7 @@ enum Cipher {
 
 impl Cipher {
     fn caesar(input: String, shift: u32) -> String {
-        let mut letters: Vec<char> = input.chars().collect();
-        let alphabet = generate_alphabet();
-
-        letters.truncate(letters.len() - 1);
-
-        let shifted_letters: Vec<char> = letters
-            .iter()
-            .map(|&letter| find_shifted_letter(letter, shift, alphabet.clone()))
-            .collect();
-
-        let shifted_word: String = shifted_letters.into_iter().collect();
-
-        return shifted_word;
+        caesar::cipher(input, shift)
     }
 }
 
